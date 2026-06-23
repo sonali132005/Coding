cc421f11d94ae1f778ff955c0142a27bd3e65a6c
Sonali Mahapatra <sonalimahapatra881@gmail.com>
2026-06-23 22:19:30 +0530

    Time: 16 ms (14%), Space: 47.5 MB (12.72%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+        String rev = new StringBuilder(s).reverse().toString();
+        if(s.equals(rev)){
+            return true;
+        }
+        return false;
+
+        
+    }
+}
